3ae21f5ca5c1b800aea911537a8f13b71d50f198
wratixor <wratixor@gmail.com>
2024-10-13 17:49:48 +0300

    do it slowly

--- db_utils/db_request.py
+++ db_utils/db_request.py
@@ -5,14 +5,71 @@ import asyncpg
 
 logger = logging.getLogger(__name__)
 
-async def join(pool: asyncpg.pool.Pool, user_id: int, group_id: int) -> str:
+async def join(pool: asyncpg.pool.Pool, user_id: int, group_id: int, username: str) -> str:
     result: str
-    val: str
     async with pool.acquire() as conn:
         try:
-            val = await conn.fetchval("select * from api.join($1::bigint, $2::bigint)", user_id, group_id)
-            result = f"join({user_id}, {group_id}) returned: {val}"
+            result = await conn.fetchval("select * from api.s_name_join($1::bigint, $2::bigint, $3::text)"
+                                         , user_id, group_id, username)
         except Exception as e:
-            result = f"join({user_id}, {group_id}) exception: {e}"
+            result = f"s_name_join({user_id}, {group_id}, {username}) exception: {e}"
             logger.error(result)
     return result
+
+async def leave(pool: asyncpg.pool.Pool, user_id: int, group_id: int, username: str) -> str:
+    result: str
+    async with pool.acquire() as conn:
+        try:
+            result = await conn.fetchval("select * from api.s_name_leave($1::bigint, $2::bigint, $3::text)"
+                                         , user_id, group_id, username)
+        except Exception as e:
+            result = f"s_name_leave({user_id}, {group_id}, {username}) exception: {e}"
+            logger.error(result)
+    return result
+
+async def kick(pool: asyncpg.pool.Pool, user_id: int, group_id: int, username: str) -> str:
+    result: str
+    async with pool.acquire() as conn:
+        try:
+            result = await conn.fetchval("select * from api.s_name_kick($1::bigint, $2::bigint, $3::text)"
+                                         , user_id, group_id, username)
+        except Exception as e:
+            result = f"s_name_kick({user_id}, {group_id}, {username}) exception: {e}"
+            logger.error(result)
+    return result
+
+async def aou_user(pool: asyncpg.pool.Pool, user_id: int, first_name: str, last_name: str, username: str
+                   , updpar: str = '', updval: str = '') -> str:
+    result: str
+    async with pool.acquire() as conn:
+        try:
+            result = await conn.fetchval("select * from api.s_aou_user($1::bigint, $2::text, $3::text, $4::text"
+                                         ", $5::text, $6::text)"
+                                         , user_id, first_name, last_name, username, updpar, updval)
+        except Exception as e:
+            result = f"s_aou_user({user_id}, {first_name}, {last_name}, {username}, {updpar}, {updval}) exception: {e}"
+            logger.error(result)
+    return result
+
+async def aou_group(pool: asyncpg.pool.Pool, group_id: int, group_type: str, group_title: str) -> str:
+    result: str
+    async with pool.acquire() as conn:
+        try:
+            result = await conn.fetchval("select * from api.s_name_kick($1::bigint, $2::text, $3::text)"
+                                         , group_id, group_type, group_title)
+        except Exception as e:
+            result = f"s_aou_group({group_id}, {group_type}, {group_title}) exception: {e}"
+            logger.error(result)
+    return result
+
+async def aou_admin(pool: asyncpg.pool.Pool, user_id: int, group_id: int, username: str, operation: str='swap') -> str:
+    result: str
+    async with pool.acquire() as conn:
+        try:
+            result = await conn.fetchval("select * from api.s_name_kick($1::bigint, $2::bigint, $3::text, $4::text)"
+                                         , user_id, group_id, username, operation)
+        except Exception as e:
+            result = f"s_aou_admin({user_id}, {group_id}, {username}, {operation}) exception: {e}"
+            logger.error(result)
+    return result
+
--- db_utils/reinit_api.sql
+++ db_utils/reinit_api.sql
@@ -2,48 +2,6 @@ DROP SCHEMA IF EXISTS api cascade;
 CREATE SCHEMA api AUTHORIZATION rmaster;
 
 
-DROP FUNCTION IF EXISTS api.s_join(int8, int8);
-CREATE OR REPLACE FUNCTION api.s_join(i_user_id bigint, i_chat_id bigint)
- RETURNS text
- LANGUAGE plpgsql
- VOLATILE SECURITY DEFINER COST 1
-AS $function$
- DECLARE
-  l_check_user boolean := false;
-  l_check_chat boolean := false;
-  l_check_isset boolean := false;
-  l_check_query boolean := false;
-
-  l_user_id bigint := coalesce(i_user_id, 0::bigint);
-  l_chat_id bigint := coalesce(i_chat_id, 0::bigint);
-
- BEGIN
-  l_check_user := ((select count(1) from rmaster.staff as u where u.user_id = l_user_id) = 1);
-  l_check_chat := ((select count(1) from rmaster.department as c where c.chat_id = l_chat_id) = 1);
-
-  IF l_check_user and l_check_chat THEN
-    l_check_isset := ((select count(1) from rmaster.staff_department as uc where uc.user_id = l_user_id and uc.chat_id = l_chat_id) = 1);
-  END IF;
-
-  IF l_check_user and l_check_chat and not l_check_isset THEN
-    insert into rmaster.staff_department (user_id, chat_id) values (l_user_id, l_chat_id);
-    l_check_query := true;
-  END IF;
-
-  RETURN (
-    SELECT
-      case when    l_check_query then 'Успешно присоединился к группе'
-           when    l_check_isset then 'Уже есть в группе'
-           when not l_check_user then 'Неизвестный пользователь'
-           when not l_check_chat then 'Неизвестная группа'
-           else 'Не выполнено' end::text as status
-    FOR READ ONLY
-  );
- END
-$function$
-;
-
-
 DROP FUNCTION IF EXISTS api.s_aou_user(int8, text, text, text, text, text);
 CREATE OR REPLACE FUNCTION api.s_aou_user(i_user_id bigint, i_first_name text, i_last_name text, i_username text, i_upd_par text default null::text, i_upd_val text default null::text)
  RETURNS text
@@ -82,11 +40,11 @@ AS $function$
     END IF;
   END IF;
 
-  IF l_check_user and l_upd_par = 'color' then
+  IF l_check_user and l_upd_par = 'color' THEN
     l_color := case when l_upd_val <> '' then l_upd_val
-                    else ((('\x'::text || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50))
-                                       || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50))
-                                       || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50))
+                    else ('\x'::text || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50)
+                                     || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50)
+                                     || to_hex(trunc(random() * 200::double precision)::integer % 200 + 50))
                     end::bytea;
     update rmaster.staff set color = l_color, update_date = now() where user_id = l_user_id;
     l_update_user := true;
@@ -176,7 +134,7 @@ AS $function$
   l_admin_id bigint := coalesce(i_admin_id, 0::bigint);
   l_chat_id bigint := coalesce(i_chat_id, 0::bigint);
   l_user_id bigint := 0;
-  l_username text := coalesce(i_user_id, '');
+  l_username text := coalesce(i_username, '');
   l_oper text := lower(coalesce(i_oper, ''));
 
  BEGIN
@@ -219,11 +177,11 @@ AS $function$
 
   RETURN (
     SELECT
-      case when l_enabled then l_username||' добавлены права администратора'
-           when l_disabled then 'У '||l_username||' отозваны права администратора'
-           when not l_check_chat then 'Неизвестный чат'
-           when not l_check_user then 'Неизвестный пользователь'
+      case when         l_enabled then l_username||' назначены права администратора'
+           when        l_disabled then 'У '||l_username||' отозваны права администратора'
            when not l_check_admin then 'Недостаточно прав'
+           when not l_check_chat  then 'Неизвестный чат'
+           when not l_check_user  then 'Неизвестный пользователь'
            else 'Не выполнено' end::text as status
     FOR READ ONLY
   );
@@ -231,4 +189,162 @@ AS $function$
 $function$
 ;
 
+
+DROP FUNCTION IF EXISTS api.s_name_join(int8, int8, text);
+CREATE OR REPLACE FUNCTION api.s_name_join(i_admin_id bigint, i_chat_id bigint, i_username text)
+ RETURNS text
+ LANGUAGE plpgsql
+ VOLATILE SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+  l_check_admin boolean := false;
+  l_check_user boolean := false;
+  l_check_chat boolean := false;
+  l_check_isset boolean := false;
+  l_check_query boolean := false;
+  l_check_flg int4 := 0;
+
+  l_user_id bigint;
+  l_chat_id bigint := coalesce(i_chat_id, 0::bigint);
+  l_username text := coalesce(i_username, '');
+
+ BEGIN
+  l_user_id := (select user_id from rmaster.staff as u where u.username = l_username limit 1);
+  l_check_chat := ((select count(1) from rmaster.department as c where c.chat_id = l_chat_id) = 1);
+  l_check_user := (l_user_id is not null);
+  l_check_admin := (l_admin_id = l_user_id) or ((select count(1)
+                                                   from rmaster.admin_department as ad
+                                                  where ad.chat_id = l_chat_id
+                                                    and ad.user_id = l_admin_id
+                                                    and enable_flg = 10) = 1);
+  l_check_flg := (select uc.enable_flg from rmaster.staff_department as uc where uc.user_id = l_user_id and uc.chat_id = l_chat_id limit 1);
+
+  IF l_check_user and l_check_chat and l_check_admin THEN
+    IF l_check_flg is null THEN
+      insert into rmaster.staff_department (user_id, chat_id) values (l_user_id, l_chat_id);
+      l_check_query := true;
+    ELSEIF l_check_flg = 12 THEN
+      update rmaster.staff_department set enable_flg = 10, update_date = now() where user_id = l_user_id and chat_id = l_chat_id;
+    END IF;
+  END IF;
+
+  RETURN (
+    SELECT
+      case when     l_check_query then 'Успешно присоединился к группе'
+           when  l_check_flg = 10 then 'Уже есть в группе'
+           when  l_check_flg = 12 then 'Восстановлен в группу'
+           when not l_check_admin then 'Недостаточно прав'
+           when not  l_check_user then 'Неизвестный пользователь'
+           when not  l_check_chat then 'Неизвестная группа'
+           else 'Не выполнено' end::text as status
+    FOR READ ONLY
+  );
+ END
+$function$
+;
+
+
+DROP FUNCTION IF EXISTS api.s_name_leave(int8, int8, text);
+CREATE OR REPLACE FUNCTION api.s_name_leave(i_admin_id bigint, i_chat_id bigint, i_username text)
+ RETURNS text
+ LANGUAGE plpgsql
+ VOLATILE SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+  l_check_admin boolean := false;
+  l_check_user boolean := false;
+  l_check_chat boolean := false;
+  l_check_isset boolean := false;
+  l_check_query boolean := false;
+  l_check_flg int4 := 0;
+
+  l_user_id bigint;
+  l_chat_id bigint := coalesce(i_chat_id, 0::bigint);
+  l_username text := coalesce(i_username, '');
+
+  l_del_admin text;
+
+ BEGIN
+  l_user_id := (select user_id from rmaster.staff as u where u.username = l_username limit 1);
+  l_check_chat := ((select count(1) from rmaster.department as c where c.chat_id = l_chat_id) = 1);
+  l_check_user := (l_user_id is not null);
+  l_check_admin := (l_admin_id = l_user_id) or ((select count(1)
+                                                   from rmaster.admin_department as ad
+                                                  where ad.chat_id = l_chat_id
+                                                    and ad.user_id = l_admin_id
+                                                    and enable_flg = 10) = 1);
+  l_check_flg := (select uc.enable_flg from rmaster.staff_department as uc where uc.user_id = l_user_id and uc.chat_id = l_chat_id limit 1);
+
+  IF l_check_user and l_check_chat and l_check_admin THEN
+    IF l_check_flg = 10 THEN
+      update rmaster.staff_department set enable_flg = 12, update_date = now() where user_id = l_user_id and chat_id = l_chat_id;
+      l_check_query := true;
+    END IF;
+  END IF;
+
+  l_del_admin := (select * from api.s_aou_admin(i_admin_id::bigint, i_chat_id::bigint, i_username::text, 'del'::text));
+
+  RETURN (
+    SELECT
+      case when       l_check_query then 'Успешно покинул группу. Права: '||l_del_admin
+           when    l_check_flg = 12 then 'Уже не в группе'
+           when l_check_flg is null then 'Отсутствует в группе'
+           when not   l_check_admin then 'Недостаточно прав'
+           when not    l_check_user then 'Неизвестный пользователь'
+           when not    l_check_chat then 'Неизвестная группа'
+           else 'Не выполнено' end::text as status
+    FOR READ ONLY
+  );
+ END
+$function$
+;
+
+
+DROP FUNCTION IF EXISTS api.s_name_kick(int8, int8, text);
+CREATE OR REPLACE FUNCTION api.s_name_kick(i_admin_id bigint, i_chat_id bigint, i_username text)
+ RETURNS text
+ LANGUAGE plpgsql
+ VOLATILE SECURITY DEFINER COST 1
+AS $function$
+ DECLARE
+  l_check_admin boolean := false;
+  l_check_user boolean := false;
+  l_check_chat boolean := false;
+  l_check_isset boolean := false;
+  l_check_query boolean := false;
+  l_check_flg int4 := 0;
+
+  l_user_id bigint;
+  l_chat_id bigint := coalesce(i_chat_id, 0::bigint);
+  l_username text := coalesce(i_username, '');
+
+ BEGIN
+  l_user_id := (select user_id from rmaster.staff as u where u.username = l_username limit 1);
+  l_check_chat := ((select count(1) from rmaster.department as c where c.chat_id = l_chat_id) = 1);
+  l_check_user := (l_user_id is not null);
+  l_check_admin := (l_admin_id = l_user_id) or ((select count(1)
+                                                   from rmaster.admin_department as ad
+                                                  where ad.chat_id = l_chat_id
+                                                    and ad.user_id = l_admin_id
+                                                    and enable_flg = 10) = 1);
+  l_check_flg := (select uc.enable_flg from rmaster.staff_department as uc where uc.user_id = l_user_id and uc.chat_id = l_chat_id limit 1);
+
+  IF l_check_user and l_check_chat and l_check_admin THEN
+      delete from rmaster.staff_department where user_id = l_user_id and chat_id = l_chat_id;
+      delete from rmaster.admin_department where user_id = l_user_id and chat_id = l_chat_id;
+      l_check_query := true;
+  END IF;
+
+  RETURN (
+    SELECT
+      case when     l_check_query then 'Исключён из группы, права администратора отозваны, если были.'
+           when not l_check_admin then 'Недостаточно прав'
+           when not  l_check_user then 'Неизвестный пользователь'
+           when not  l_check_chat then 'Неизвестная группа'
+           else 'Не выполнено' end::text as status
+    FOR READ ONLY
+  );
+ END
+$function$
+;
 
