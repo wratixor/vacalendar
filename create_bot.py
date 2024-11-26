b9118c587370b11597618bfc289daa47629bab6d
wratixor <wratixor@gmail.com>
2024-11-26 17:13:22 +0300

    Prod

--- create_bot.py
+++ create_bot.py
@@ -2,13 +2,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-#from aiogram.fsm.storage.redis import RedisStorage
-from aiogram.fsm.storage.memory import MemoryStorage
+#from aiogram.fsm.storage.memory import MemoryStorage
+from aiogram.fsm.storage.redis import RedisStorage
 from decouple import config
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-storage: MemoryStorage = MemoryStorage()
-#storage: RedisStorage = RedisStorage.from_url(config('REDIS_LINK_0'))
+logging.basicConfig(level=logging.ERROR, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+#storage: MemoryStorage = MemoryStorage()
+storage: RedisStorage = RedisStorage.from_url(config('REDIS_LINK_0'))
 
 admins: set = set(int(admin_id) for admin_id in config('ADMINS').split(','))
 pg_link: str = config('PG_LINK')
